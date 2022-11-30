95c2d4262f174e1e855ca4be66ca13384af0d4cc
Ben Madley <ben.madley.contractor@dvla.gov.uk>
2022-11-30 10:01:02 +0000

    First idea.

---
+++ js/1_GuidedExercise/run_tests.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def run_tests
+  value = `npm test`
+  puts value
+  $?.exitstatus
+end
+
+result = run_tests
+while result === 0
+  puts "\e[32mPASSING\e[0m"
+  sleep 1
+  result = run_tests
+end
+
+puts "\e[31mTESTS FAILED!\e[0m"
---
+++ js/1_GuidedExercise/src/idea_01.js
@@ -0,0 +1,8 @@
+// Task: Rename word to something better.
+const rename_a_bad_variable = (word) => {
+  const thing = ' '
+
+  return word.includes(thing)
+}
+
+module.exports = { rename_a_bad_variable }
